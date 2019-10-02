797a5023209685cac302cef18ee6399681f7a014
Pelle Lindroth <pellelindroth@gmail.com>
2019-10-02 19:07:20 +0200

    Updated tests

--- client/src/tests/components/HighscoreList.test.js
+++ client/src/tests/components/HighscoreList.test.js
@@ -5,21 +5,25 @@ import HighscoreList from '../../components/highscores/HighscoreList';
 
 let highscores = [
   {
+    _id: 'one',
     date: 1567247152162.0,
     score: 97,
     player: 'falle'
   },
   {
+    _id: 'two',
     date: 1568387025126.0,
     score: 118,
     player: 'babs'
   },
   {
+    _id: 'three',
     date: 1568447073138.0,
     score: 123,
     player: 'mob pete'
   },
   {
+    _id: 'four',
     date: 1568734844461.0,
     score: 147,
     player: 'krille k'
