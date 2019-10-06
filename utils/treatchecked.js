be4ee4cb3ba2fad0948f3cbe9b3af2d27ca986f9
Pelle Lindroth <pellelindroth@gmail.com>
2019-10-06 21:18:39 +0200

    Added filtering for checked bands without profile img

--- utils/treatchecked.js
+++ utils/treatchecked.js
@@ -6,38 +6,46 @@ const treatChecked = (userBand, bands) => {
   if (bands.length > 5) {
     bands.length = 5;
   }
-  for (let i = 0; i < bands.length; i++) {
-    if (bands[i].title.includes(')')) {
-      let split = bands[i].title.split('(');
-      split = split[0];
-      bands[i].title = split.trim();
+
+  bands = bands.filter(band => {
+    return !band.cover_image.match(/^.*spacer\.gif$/);
+  });
+
+  if (bands.length) {
+    for (let i = 0; i < bands.length; i++) {
+      if (bands[i].title.includes(')')) {
+        let split = bands[i].title.split('(');
+        split = split[0];
+        bands[i].title = split.trim();
+      }
     }
-  }
-
-  if (
-    userBand.includes('the') ||
-    bands[0].title.toLowerCase().includes('the')
-  ) {
-    userBand = theFix(userBand, bands[0].title.toLowerCase());
-  }
-
-  userBand = dotFix(userBand);
-
-  let nameMatches = [];
-  let imgMatches = [];
-
-  for (let i = 0; i < bands.length; i++) {
-    bands[i].title = bands[i].title.toLowerCase();
-    if (bands[i].title === userBand) {
-      nameMatches.push(bands[i].title);
-      imgMatches.push(bands[i].cover_image);
+    if (
+      userBand.match(/^the\\s/) ||
+      bands[0].title.toLowerCase().match(/^the\\s/)
+    ) {
+      userBand = theFix(userBand, bands[0].title.toLowerCase());
     }
-  }
-  if (nameMatches.length) {
-    return {
-      name: nameMatches[0],
-      imgUrl: imgMatches[0]
-    };
+
+    userBand = dotFix(userBand);
+
+    let nameMatches = [];
+    let imgMatches = [];
+
+    for (let i = 0; i < bands.length; i++) {
+      bands[i].title = bands[i].title.toLowerCase();
+      if (bands[i].title === userBand) {
+        nameMatches.push(bands[i].title);
+        imgMatches.push(bands[i].cover_image);
+      }
+    }
+    if (nameMatches.length) {
+      return {
+        name: nameMatches[0],
+        imgUrl: imgMatches[0]
+      };
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
