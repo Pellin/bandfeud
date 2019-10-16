196be630fa05258826a9d91b27939ae064fe40f1
Pelle Lindroth <pellelindroth@gmail.com>
2019-10-16 20:43:15 +0200

    deleteText function that clears input on Backspace press

--- client/src/components/home/play/SubmitBand.js
+++ client/src/components/home/play/SubmitBand.js
@@ -13,14 +13,18 @@ export const SubmitBand = props => {
   const onRemovePlaceholder = () => {
     setIosPlaceHold('');
   };
-  const onBandChange = event => {
-    let bandName = event.target.value;
+  const onBandChange = e => {
+    let bandName = e.target.value;
     if (
       bandName.match(/^[a-zåäö0-9/\-&-,.\s]*$/) &&
       !bandName.match(/^[.,-\s]/)
     ) {
       setBand(bandName);
     }
+  };
+  const deleteText = e => {
+    if (e.key !== 'Backspace') return;
+    setBand('');
   };
   const onSubmit = e => {
     e.preventDefault();
@@ -57,18 +61,23 @@ export const SubmitBand = props => {
         onSubmit={onSubmit}
       >
         <input
-          className={band.length > 25 && props.os !== 'desktop' ? 'band-input-long' : "band-input" }
-          type="text"
+          className={
+            band.length > 25 && props.os !== 'desktop'
+              ? 'band-input-long'
+              : 'band-input'
+          }
+          autoCapitalize="none"
           autoComplete="off"
           autoCorrect="off"
-          autoCapitalize="none"
           autoFocus={props.os !== 'desktop' ? false : true}
           maxLength="42"
-          value={band}
           onChange={onBandChange}
+          onFocus={() => onRemovePlaceholder()}
+          onKeyDown={deleteText}
           placeholder={props.os !== 'desktop' ? iosPlaceHold : undefined}
           spellCheck={false}
-          onFocus={() => onRemovePlaceholder()}
+          type="text"
+          value={band}
         />
       </form>
       {props.previous.previous2 ? (
@@ -83,92 +92,6 @@ export const SubmitBand = props => {
     </>
   );
 };
-
-// export class SubmitBand extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       band: '',
-//       iosPlaceHold: 'press to type'
-//     };
-//   }
-//   onSetPlaceholder = () => {
-//     this.setState(() => ({ iosPlaceHold: '' }));
-//   };
-//   onBandChange = event => {
-//     let band = event.target.value;
-//     if (band.match(/^[a-zåäö0-9/\-&-,.\s]*$/) && !band.match(/^[.,-\s]/)) {
-//       this.setState(() => ({ band }));
-//     }
-//   };
-//   onSubmit = e => {
-//     e.preventDefault();
-//     if (this.state.band.length < 2 || !this.state.band.match(/[\wåäö]$/))
-//       return;
-
-//     this.props.onCheckBand(
-//       this.state.band,
-//       this.props.previous,
-//       this.props.used,
-//       this.props.bandBank,
-//       this.props.score,
-//       this.props.difficulty
-//     );
-//     this.setState(() => ({ band: '' }));
-//     this.props.onSetDifficulty(this.props.used.length);
-//   };
-//   render() {
-//     return (
-//       <>
-//         {this.props.os !== 'desktop' && (
-//           <div className="footer-counter-container">
-//             <Counter
-//               submitted={this.props.submitted}
-//               inGame={this.props.inGame}
-//               used={this.props.used}
-//               difficulty={this.props.difficulty}
-//               score={this.props.score}
-//             />
-//           </div>
-//         )}
-//         <form
-//           className={
-//             this.props.os === 'iOS' ? 'band-input-form-ios' : 'band-input-form'
-//           }
-//           onSubmit={this.onSubmit}
-//         >
-//           <input
-//             className="band-input"
-//             type="text"
-//             inputMode="email"
-//             autoComplete="off"
-//             autoCorrect="off"
-//             autoCapitalize="none"
-//             autoFocus={this.props.os !== 'desktop' ? false : true}
-//             maxLength="25"
-//             value={this.state.band}
-//             onChange={this.onBandChange}
-//             placeholder={
-//               this.props.os !== 'desktop' ? this.state.iosPlaceHold : undefined
-//             }
-//             onFocus={() => this.onSetPlaceholder()}
-//           />
-//         </form>
-//         {this.props.previous.previous2 ? (
-//           <div className="previous">
-//             {this.props.previous.previous1.toUpperCase() +
-//               ' or ' +
-//               this.props.previous.previous2.toUpperCase()}
-//           </div>
-//         ) : (
-//           <div className="previous">
-//             {this.props.previous.previous1.toUpperCase()}
-//           </div>
-//         )}
-//       </>
-//     );
-//   }
-// }
 
 const mapDispatchToProps = dispatch => ({
   onCheckBand: (band, previous, used, bandBank, score, difficulty) =>
