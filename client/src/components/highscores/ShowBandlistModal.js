884b51447f5f6f8f90a363692cdba1cc58e0f90e
Pelle Lindroth <pellelindroth@gmail.com>
2019-11-24 18:10:26 +0100

    Add fail reason as last element to bands

--- client/src/components/highscores/ShowBandlistModal.js
+++ client/src/components/highscores/ShowBandlistModal.js
@@ -3,12 +3,23 @@ import Modal from 'react-modal';
 
 import moment from 'moment';
 
+import Stopwatch from '../../icons/Stopwatch';
+
 Modal.setAppElement(document.getElementById('root'));
 
 const ShowBandlistModal = props => {
-  const goToDiscogs = band => {   
+  const points = [];
+  if (props.showModal && props.lastRound) {
+    for (let band of props.bands) {
+      if (band.points) {
+        points.push(band.points);
+      }
+    }
+  }
+  const goToDiscogs = band => {
+    if (band.mode) return;
     if (band.id) {
-      window.open(`https://www.discogs.com/artist/${band.id}`)
+      window.open(`https://www.discogs.com/artist/${band.id}`);
     } else {
       if (band.name === 'n.w.a') {
         band.name = 'n.w.a.';
@@ -27,46 +38,76 @@ const ShowBandlistModal = props => {
       className="modal"
       overlayClassName="overlay"
     >
-      {props.playerInfo && (
+      <div className="modal-header">
+        {props.playerInfo && (
+          <>
+            <div className="modal-header-left">
+              {moment(props.playerInfo.date).format('DD/MM/YYYY')}{' '}
+            </div>
+            <div className="header-player">
+              <div className="header-stopwatch">
+                <Stopwatch />
+              </div>
+              {props.playerInfo.player}
+              <p>{props.bands.length / 2} bands</p>
+            </div>
+            <div className="modal-header-right">{props.playerInfo.score}p</div>
+          </>
+        )}
+      </div>
+      {props.lastRound && (
         <div className="modal-header">
-          <div className="header-date">
-            {moment(props.playerInfo.date).format('DD/MM/YYYY')}{' '}
-          </div>{' '}
-          <div className="header-player">
-            {props.playerInfo.player}
-            <p>{props.bands.length / 2} bands</p>
-          </div>{' '}
-          <div className="header-points">{props.playerInfo.score}p</div>
+          <div className="last-round-info">
+            {points.length && points.reduce((acc, curr) => acc + curr)}p
+            <p>
+              {props.bands.length / 2 - 0.5}{' '}
+              {props.bands.length / 2 - 0.5 === 1 ? 'band' : 'bands'}
+            </p>
+          </div>
         </div>
       )}
-      {props.showModal &&
-        props.bands.map(band => (
-          <div className="band-info" key={band.name}>
-            <div className="band-image-container">
-              <img
-                title={`Open Discogs page for ${band.name.toUpperCase()} `}
-                onClick={() => goToDiscogs(band)}
-                className="band-image"
-                src={band.url}
-                alt={band.name}
-              />
-            </div>
-            <div className="band-name-container">
-            {band.points ? (
-              <div className="band-points">{band.points}p</div>
-            ) : (
-              <div className="no-band-points">No points</div>
-            )}
-              <div className={band.points ? 'band-name' : 'band-name-computer'}>
-                {band.name}
+      <div className="modal-flow">
+        {props.showModal &&
+          props.bands.map(band => (
+            <div className="band-info" key={band.mode ? band.mode : band.name}>
+              <div className="band-image-container">
+                {band.url ? (
+                  <img
+                    title={`Open Discogs page for ${band.name.toUpperCase()} `}
+                    onClick={() => goToDiscogs(band)}
+                    className="band-image"
+                    src={band.url}
+                    alt={band.name}
+                  />
+                ) : (
+                  <div className="fail-svg">
+                    <Stopwatch />
+                  </div>
+                )}
               </div>
-          
+              <div className="band-name-container">
+                {band.points ? (
+                  <div className="band-points">{band.points}p</div>
+                ) : band.mode ? (
+                  <div className="band-points">{band.mode}</div>
+                ) : (
+                  <div className="no-band-points">No points</div>
+                )}
+                <div
+                  className={
+                    band.points
+                      ? 'band-name'
+                      : band.url
+                      ? 'band-name-computer'
+                      : 'band-name-failed'
+                  }
+                >
+                  {band.name}
+                </div>
+              </div>
             </div>
-
-          
-          </div>
-          
-        ))}
+          ))}
+      </div>
     </Modal>
   );
 };
