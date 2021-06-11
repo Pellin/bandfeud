60c8337111c9f52cd1b357426458ff608c835242
PelleLindroth <pellelindroth@gmail.com>
2021-06-11 16:35:26 +0200

    Add path to sitemap

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   const path = require('path');
+
+  app.get("/sitemap.xml", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'sitemap.xml'));
+  });
   app.get('*', (_req, res) => {
     res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
   });
